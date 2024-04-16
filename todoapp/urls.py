1f5fb5c2f8cd6e1a3fb863139e5a3c0ec02327e5
Santosh <santoshmjadhav1994@gmail.com>
2024-04-16 23:27:36 +0530

    final commit

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -5,5 +5,16 @@ from todoapp import views
 
 
 urlpatterns = [
+    # add task
     path('addTask/',views.addTask,name='addTask'),
+
+    path('mark_as_done/<int:pk>/',views.mark_as_done,name='mark_as_done'),
+
+    path('mark_as_undone/<int:pk>/',views.mark_as_undone,name='mark_as_undone'),
+
+    # Edit Feature
+    path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
+
+    # delete task
+    path('delete_task/<int:pk>/',views.delete_task,name='delete_task'),
 ]
